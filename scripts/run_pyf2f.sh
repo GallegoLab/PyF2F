fca6691fd9e134f262899477eb7f82c4c56d872e
Live-Cell Structural Biology lab <102747243+GallegoLab@users.noreply.github.com>
2023-08-02 15:41:59 +0200

    Update run_pyf2f.sh

--- scripts/run_pyf2f.sh
+++ scripts/run_pyf2f.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/bash
+# Description: This is a bash script to run the image registration
+# protocol of PyF2F. The parameters below are adequated for a camera 
+# of pixel size = 64.5 nm
 
 # PARAMETERS 
-directory=$1           # name of your sample directory
-px_size=64.5           # pixel size in nm (Zyla: 65; Prime: 110)
-rb_radius=70           # Rolling ball radius
+directory=$1               # name of your sample directory
+px_size=64.5               # pixel size in nm (Zyla: 65; Prime: 110)
+rb_radius=70               # Rolling ball radius
 md_radius=10		   # Median filter radius
-diameter=11			   # spot detection: diameter of spots in px
+diameter=11	           # spot detection: diameter of spots in px
 percentile=99.7		   # spot detection: sort spots below this percentile of intensity (select only the brightest spots)
-max_displacement=2      # linking: link spots from W1-W2 channels separated by a 'max_displacement' in px
+max_displacement=2         # linking: link spots from W1-W2 channels separated by a 'max_displacement' in px
 contour_cutoff=13	   # segmentation: select spots falling no far from this cutoff to the cell contour
 neigh_cutoff=9	  	   # segmentation: select spots falling at least this cutoff from the closest neighbour
 kde_cutoff=0.5
 gaussian_cutoff=0.35
 mle_cutoff=0.66		   # MLE: discard outliers found in the right 1 - mle_cutoff tail area of the distance distribution
-bin_size=5                # Bin size for distance distribution plots
+bin_size=5                 # Bin size for distance distribution plots
 option=$2		   # Option to run pyf2f (see README.md) all or spt,warping,segment,kde,gaussian,mle
 
 
